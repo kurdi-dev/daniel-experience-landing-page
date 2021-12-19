123ed81d0fe6f29054466b77882d98d0c45bc8a7
Walid <walid.r.rashed@gmail.com>
2021-12-19 16:54:36 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,58 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className='flex flex-col items-center font-popins bg-[#F3EBE6] justify-center min-h-screen'>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>The Daniel Experience</title>
       </Head>
+      <header
+        className='w-full h-28 md:h-36 lg:h-48 bg-bottom  bg-cover'
+        style={{
+          backgroundImage: "url('/header-bg.png')",
+          backgroundPositionX: "left",
+        }}
+      >
+        <div className='text-lg w-full h-full flex items-center justify-center md:justify-start text-white'>
+          <img
+            className='w-auto  h-12 lg:h-16 ml-10 mb-5'
+            src='/logo.png'
+            alt='logo'
+          />
+        </div>
+      </header>
 
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
-
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
-
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
+      <main className='flex flex-col items-center justify-center w-full flex-1 px-20 text-center'>
+        <div className='container mx-auto text-left'>
+          <div className='mb-5'>
+            <h1 className='md:text-6xl text-4xl font-bold text-main mb-5'>
+              Ahoy!
+            </h1>
+            <p className='md:text-lg text-base'>
+              Thank you for being part of my journey! I am still building my
+              website, finding solutions to some new real-world problems that
+              users are facing.
             </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
+          </div>
+          <div className='mb-5'>
+            <h1 className='md:text-2xl text-xl font-bold text-main mb-1'>
+              Meanwhile
+            </h1>
+            <p className='md:text-lg text-base'>
+              Feel free to contact me or download my profile:
             </p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+            <p className='md:text-lg text-base'>Hi@thedanielexperience.com</p>
+            <p className='md:text-lg text-base'>+964 750 244 9783</p>
+          </div>
+          <button className='btn btn-main'>Download Portfolio</button>
         </div>
       </main>
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+      <footer className='flex items-center justify-center w-full h-24 border-t'>
+        <p className='md:text-base text-xs'>
+          The Daniel Experience - All Rights Reserverd
+        </p>
       </footer>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: "#B42A26",
+      },
+      fontFamily: {
+        popins: ["Poppins", "Helvetica", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
